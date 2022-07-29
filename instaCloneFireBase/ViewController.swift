556928fbf76e1f19745e8c1e2c1a2b556092a38b
alperenkavukk <kavuk.67.76@gmail.com>
2022-07-29 21:29:46 +0300

    Initial Commit

---
+++ instaCloneFireBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instaCloneFireBase
+//
+//  Created by Alperen Kavuk on 29.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
